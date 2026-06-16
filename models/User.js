ad84554779aa2e97dcd21e75d12c86e32abc5688
reisjoaov <joaoreisvictor@hotmail.com>
2026-06-16 19:16:42 -0300

    adaptação de tema para aula 4

--- models/User.js
+++ models/User.js
@@ -2,35 +2,36 @@ const { Sequelize, DataTypes } = require("sequelize");
 
 const db = require("../database/conn");
 
-const Roledev = db.define("Roledev", {
+const User = db.define("User", {
   id: {
     type: DataTypes.INTEGER,
-    primaryKey: true,
     autoIncrement: true,
+    primaryKey: true,
   },
-
   name: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true,
   },
-
-  level: {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  age: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  phoneNumber: {
     type: DataTypes.STRING,
     allowNull: false,
   },
-
-  description: {
+  country: {
     type: DataTypes.STRING,
-    allowNull: true,
-  },
-
-  label: {
-    type: DataTypes.VIRTUAL,
-    get() {
-      return `${this.name} - ${this.level}`;
-    },
+    allowNull: false,
   },
 });
 
-module.exports = Roledev;
+module.exports = User;
