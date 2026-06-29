c50a5bbf9269c846cab20eb03e9b9fea7a08c7e8
reisjoaov <joaoreisvictor@hotmail.com>
2026-06-28 22:02:43 -0300

    Banco de daddos remoto configurado.

--- database/conn.js
+++ database/conn.js
@@ -1,12 +1,20 @@
 const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
 
-const sequelize = new Sequelize("infdb", "root", "123", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mariadb",
+const sequelize = new Sequelize("Projeto_nodejs-sql", "dbpwf26264703", "MEi2lRe1N4jH6dw3f[mWa", {
+  host: 'serverless-us-east-1.sysp0000.db1.skysql.com',
+  port: 4034,
+  dialect: 'mariadb',
   define: {
     freezeTableName: true,
   },
+  dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync(path.join(__dirname, "../config/globalsignrootca.pem")),
+      },
+    },
+    logging: false,
 });
 
 try {
